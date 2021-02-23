a8bf92ac55027f60750b59cd3a2758beea8ebbaf
Aman Kumar Gautam <aman15091999@gmail.com>
2021-02-23 13:20:18 +0530

    main-b

--- hungry.py
+++ hungry.py
@@ -2,6 +2,6 @@ hungry=input("Are you hungry ?")
 if hungry=='yes':
     print("Eat Samosa & Pizza & Drink Lassi")
     print("Eat Chole Bhature")
+    print("This is Main Branch")
 else:
     print("Do your homework")
-    print("This is Main Branch")
